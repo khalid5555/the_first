d3451daca6b5366aa176065245fc439830562dae
khalid5555 <khalid.assuity@yahoo.com>
2022-11-16 22:41:00 +0200

    add firebase to project...

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -7,6 +8,8 @@ import 'package:the_first/screen/singUp_view.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
